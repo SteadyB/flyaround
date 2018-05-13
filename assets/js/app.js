f043d9e417786b149849d5b7477faba69d204bd9
SteadyB_Benjamin <benjamin.berthier.mail@gmail.com>
2018-05-13 15:17:47 +0200

    swap to BS4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,7 @@
 var $ = require('jquery');
 // JS is equivalent to the normal "bootstrap" package
 // no need to set this to a variable, just require it
-require('bootstrap-sass');
-
-// or you can include specific pieces
-// require('bootstrap-sass/javascripts/bootstrap/tooltip');
-// require('bootstrap-sass/javascripts/bootstrap/popover');
+require('bootstrap');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
